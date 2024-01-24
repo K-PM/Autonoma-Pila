d0bf04342d0c17b8e2375ca765af7134450f581d
K-PM <kristellperez34@gmail.com>
2024-01-24 03:07:42 -0600

    add lexenas

---
+++ src/page/index.js
@@ -0,0 +1,62 @@
+class Lexer {
+    constructor(input) {
+      this.input = input;
+      this.position = 0;
+      this.tokenTable = [
+        { regex: /funcion/, type: 'FUNCION' },
+        { regex: /[a-z]+/, type: 'NOMBRE' },
+        { regex: /int|string|float|bool/, type: 'TIPO' },
+        { regex: /return/, type: 'RETURN' },
+        { regex: /for/, type: 'FOR' },
+        { regex: /if/, type: 'IF' },
+        { regex: /\(/, type: 'ABRIR_PARENTESIS' },
+        { regex: /\)/, type: 'CERRAR_PARENTESIS' },
+        { regex: /\{/, type: 'ABRIR_CORCHETE' },
+        { regex: /\}/, type: 'CERRAR_CORCHETE' },
+        { regex: /:/, type: 'DOS_PUNTOS' },
+        { regex: /;/, type: 'PUNTO_COMA' },
+        { regex: /,/, type: 'COMA' }, // Agregado para manejar la coma
+        { regex: /[0-9]+/, type: 'DIGITO' },
+        { regex: /(<|>|<=|>=|!=|==)/, type: 'OPERADOR' },
+      ];
+    }
+  
+    getNextToken() {
+      while (this.position < this.input.length) {
+        let char = this.input[this.position];
+        for (const tokenDef of this.tokenTable) {
+          const match = this.input.slice(this.position).match(tokenDef.regex);
+          if (match && match.index === 0) {
+            this.position += match[0].length;
+            return { type: tokenDef.type, value: match[0] };
+          }
+        }
+  
+        // Ignorar espacios en blanco
+        if (/\s/.test(char)) {
+          this.position++;
+          continue;
+        }
+  
+        // Si no coincide con ninguno de los tokens conocidos, lanzar un error
+        throw new Error(`Caracter inesperado: ${char}`);
+      }
+  
+      // Fin del código fuente, devolver token de fin
+      return { type: 'EOF', value: null };
+    }
+  
+    analyze() {
+      let token = this.getNextToken();
+      while (token.type !== 'EOF') {
+        console.log(token);
+        token = this.getNextToken();
+      }
+    }
+  }
+  
+  // Ejemplo de uso
+  const input = 'variable:sa';
+  const lexer = new Lexer(input);
+  lexer.analyze();
+  
